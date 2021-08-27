7ef53299e43c210bf012d2690e3aa22bf9d69f32
Juan Antonio Ortega Rejón <juan.ortega@ibistic.com>
2021-08-27 12:51:12 +0200

    Added message from db

--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -5,11 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,7 +23,23 @@ namespace WebApplication1.Pages
 
         public void OnGet()
         {
+            using (var cn = new SqlConnection("Server=tcp:ibisticdockerexample.database.windows.net,1433;Initial Catalog=docker;Persist Security Info=False;User ID=docker;Password=Mercell0;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                using (var cmd = new SqlCommand() { Connection = cn, CommandText = "select @@VERSION" })
+                {
+                    try
+                    {
+                        cn.Open();
 
+                        var reader = cmd.ExecuteReader();
+                        reader.Read();
+                        Message = reader.GetString(0);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+            }
         }
     }
 }
